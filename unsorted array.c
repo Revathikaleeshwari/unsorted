617361e053ae6ccae7c220b29d0b777bd50f91b5
Revathikaleeshwari <nrevathibe38@gmail.com>
2016-06-28 13:59:06 +0530

    Rename unsorted.c to unsorted array.c

---
+++ unsorted array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int array[100],size,i,j;
+printf("Enter number of elementsin array\n");
+scanf("%d",&size);
+printf("Enter %d number\n",size);
+for(i=0;i<size;i++)
+{
+scanf("%d",&array[i]);
+}
+printf("Unique element\n");
+for(i=0;i<size;i++)
+{
+for(j=0;j<i;j++)
+{
+if(array[i]==array[j])
+break;
+}
+if(i==j)
+{
+printf("%d",array[i]);
+}
+}
+getch();
+return 0;
+}
